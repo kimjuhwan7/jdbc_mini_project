9fc5c5a0fadf5c39ad2061ae24d5378bbe1b965f
김주환 <juhwan71047@gmail.com>
2022-10-24 15:43:04 +0900

    Hi99_Update2

--- javaproject/src/game/Hi99.java
+++ javaproject/src/game/Hi99.java
@@ -12,20 +12,23 @@ public class Hi99 extends Thread {
 	static int count_down = 0;// 카운트 다운
 	static int score;// 총점
 	static int plus = 10;// 추가점수
+	static boolean situation = true;
+	static int start_count = 10;// 잔여시간
 
 	@Override
 	public void run() {
-System.out.println("김");
-		for (int start_count = 5; start_count < count_down; start_count--) {
+
+		System.out.println("run() 들어옴");
+		for (; start_count > count_down - 1; start_count--) {
 			try {
 				Thread.sleep(1000);
-				System.out.print(start_count);
+
 				if (count_down == start_count) {
-					System.out.println("시간 초과");
+
+					situation = false;
 				}
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
 			}
 		}
 
@@ -34,34 +37,38 @@ System.out.println("김");
 	public static void main(String[] args) {
 		Random random = new Random();
 		Scanner sc = new Scanner(System.in);
+		Hi99 thread = new Hi99();
 
-		System.out.println("구구단 게임을 시작합니다." + 5 + "초 안에 입력해주세요");
+		thread.start();
+		System.out.println("구구단 게임을 시작합니다." + start_count + "초 동안 문제를 빠르게 풀어주세요");
+
 		while (true) {
-			Hi99 thread = new Hi99();
 
-			x = random.nextInt(9) + 1;
-			y = random.nextInt(9) + 1;
+			x = random.nextInt(2) + 1;
+			y = random.nextInt(2) + 1;
 			answer = x * y;
 
-			System.out.print(x + "x" + y + "=>");
+			System.out.print(x + " x " + y + " => ");
 
-			thread.start();// 5초 타이머
-
+//----------------------------------------------------
+//첫번째 실행 후 사용자가 입력하면 첫번째 스레드를 멈추고    결과 출력 후 2번째 스레드 시작해야함
+// ----------------------------------------------------
 			z = sc.nextInt();// 사용자 입력
 
 			if (z == answer) {
 				score += 10;
 				System.out.println("정답입니다! " + plus + "점 추가 " + " 총점 =>" + score);
-				continue;
+
 			} else if (z != answer) {
 				System.out.println("오답입니다! 정답은 : " + answer + "입니다");
 
 			}
-			if (thread.isAlive()) {
-
-				thread.interrupt();
-
+			if (situation == false) {
+				System.out.println("시간이 종료 되었습니다.");
+				System.out.println("총 스코어 " + score + "입니다. 축하합니다");
+				break;
 			}
+
 		}
 	}
 
