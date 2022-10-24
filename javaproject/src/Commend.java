9fd91c9138d534d2b1ed183e08dda0cb0984d58b
김주환 <juhwan71047@gmail.com>
2022-10-24 17:30:08 +0900

    Main

---
+++ javaproject/src/Commend.java
@@ -0,0 +1,17 @@
+import java.lang.ModuleLayer.Controller;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Commend {
+	public Map<Integer, Controller> commend;
+
+	public Commend() {
+		commend = new HashMap<>();
+
+		// commend.put(1, new 로그인());
+		// commend.put(2, new 회원가입());
+		// commend.put(3, new 프로그램종료());
+
+	}
+
+}
