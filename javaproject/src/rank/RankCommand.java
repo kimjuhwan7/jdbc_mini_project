49bfefa0d47f0f12ba7023db250759fc157a9d0b
dwkang <ejddjsl123@naver.com>
2022-10-24 15:46:45 +0900

    wip

--- javaproject/src/rank/RankCommand.java
+++ javaproject/src/rank/RankCommand.java
@@ -1,14 +1,34 @@
 package rank;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import generic.GenericCommand;
+
 public class RankCommand {
 
-	private RankCommand()
-	{
-		RankCommand Instance = RankCommand;
-	}
+	List<GenericCommand> command;
+	
+	private static RankCommand instance = new RankCommand();
 	
 	RankCommand GetInstance()
 	{
-		this.Instance;
+		if(instance == null)
+			instance = new RankCommand();
+		
+		return instance;
+	}
+	
+	private RankCommand()
+	{
+		command = new LinkedList<>();
+		if(!command.add(new RankInsert()))
+		{
+			System.out.println("명령 추가 실패");
+		}
+		if(!command.add(new RankDelete()))
+		{
+			System.out.println("명령 추가 실패");			
+		}
 	}
 }
