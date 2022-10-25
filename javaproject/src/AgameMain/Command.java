65dbcf0e2d3cc7a9b7eedbb105dbe907f00dd60f
jennakim1007 <jennakim1007@gmail.com>
2022-10-25 16:07:36 +0900

    Controller
    Controller 인터페이스 수정

--- javaproject/src/AgameMain/Command.java
+++ javaproject/src/AgameMain/Command.java
@@ -21,9 +21,6 @@ public class Command {
 	public Map<String, List<Controller>> command;
 		
 
-
-
-
 	public Command() {
 		command = new HashMap<>();
 		List<Controller> menu = new ArrayList<>();
@@ -36,21 +33,24 @@ public class Command {
 		menu.add(new InsertController()); // 회원가입
 		menu.add(new ExitController()); // 프로그램 종료
 		
-//		command.put("user", user); // 분기2 : 회원 메뉴
-//		user.add(null);
-//		// user.add(new GameController()); // 게임시작
-//		user.add((Controller) new RankResearchController()); // 랭크확인
-//		user.add((Controller) new Log_outController()); // 로그아웃 -> 메인메뉴로 이동
-//		user.add((Controller) new UpdateController()); // 회원수정
-//		user.add((Controller) new DeleteController()); // 회원탈퇴
+		command.put("user", user); // 분기2 : 회원 메뉴
+		user.add(null);
+		// user.add(new GameController()); // 게임시작
+		// user.add((Controller) new RankResearchController()); // 랭크확인
+		// user.add((Controller) new Log_outController()); // 로그아웃 -> 메인메뉴로 이동
+		user.add(new Log_outController());
+		// user.add((Controller) new UpdateController()); // 회원수정
+		user.add(new UpdateController()); // 회원수정
+
 		
 //		command.put("admin", admin); // 분기2 : 관리자 메뉴
 //		admin.add(null);
 //		admin.add((Controller) new UpdateController()); // 회원관리
-//		admin.add((Controller) new RankResearchController()); // 랭크검색
+//		// admin.add((Controller) new RankResearchController()); // 랭크검색
 //		admin.add((Controller) new RankDeleteController()); // 랭크삭제
 //		admin.add((Controller) new RankInsertController()); // 랭크수정
 //		admin.add((Controller) new Log_outController()); // 로그아웃 -> 메인메뉴로 이동 
+		
 		
 		
 	}
