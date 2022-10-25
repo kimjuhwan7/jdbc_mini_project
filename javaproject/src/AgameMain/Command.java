39b9347c4935c770e1c0ea8929178a933fb3dc41
jennakim1007 <jennakim1007@gmail.com>
2022-10-25 15:16:41 +0900

    Login
    로그인 이후 분기 추가 수정

--- javaproject/src/AgameMain/Command.java
+++ javaproject/src/AgameMain/Command.java
@@ -31,18 +31,21 @@ public class Command {
 		List<Controller> admin = new ArrayList<>();
 
 		command.put("menu", menu); // 분기1 : 메인 메뉴
+		menu.add(null);
 		menu.add(new LogInController()); // // 로그인
 		menu.add(new InsertController()); // 회원가입
 		menu.add(new ExitController()); // 프로그램 종료
 		
 //		command.put("user", user); // 분기2 : 회원 메뉴
+//		user.add(null);
 //		// user.add(new GameController()); // 게임시작
 //		user.add((Controller) new RankResearchController()); // 랭크확인
 //		user.add((Controller) new Log_outController()); // 로그아웃 -> 메인메뉴로 이동
 //		user.add((Controller) new UpdateController()); // 회원수정
 //		user.add((Controller) new DeleteController()); // 회원탈퇴
-//		
+		
 //		command.put("admin", admin); // 분기2 : 관리자 메뉴
+//		admin.add(null);
 //		admin.add((Controller) new UpdateController()); // 회원관리
 //		admin.add((Controller) new RankResearchController()); // 랭크검색
 //		admin.add((Controller) new RankDeleteController()); // 랭크삭제
