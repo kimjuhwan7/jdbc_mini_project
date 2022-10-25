0eaa352f26a0178c151e905c45634c68547db33b
김주환 <juhwan71047@gmail.com>
2022-10-25 17:07:49 +0900

    Hi99수정

--- javaproject/src/game/Hi99.java
+++ javaproject/src/game/Hi99.java
@@ -2,6 +2,7 @@ package game;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import AgameMain.Main;
 import rank.controller.RankInsertController;
@@ -10,31 +11,38 @@ import rank.controller.RankInsertController;
 //hi.hi_start();
 
 public class Hi99 extends Thread {
+	Random random = new Random();
+	Scanner sc = new Scanner(System.in);
+	String pattern = "^[0-9]*$";// 숫자만
+	String val = "123456789"; // 대상문자열
 
 	public static void main(String[] args) {
 		Hi99 hi99 = new Hi99();
 		hi99.hi_start();
 	}
 
-	static int x;// 앞자리
-	static int y;// 뒷자리
-	static int z;// 사용자입력값
-	static int answer;// 앞*뒤=결과값
+	int x;// 앞자리
+	int y;// 뒷자리
+	String z;// 사용자입력값
+	int answer;// 앞*뒤=결과값
 	static int score;// 총점
-	static int plus = 10;// 추가점수
-	static int count_down = 0;// 카운트 다운
-	static int start_count = 30;// 잔여시간
+	int plus = 10;// 추가점수
+	int count_down = 0;// 카운트 다운
+	int start_count = 30;// 잔여시간
 	static int serial_num = 2;// 게임 시리얼넘버
 
 	@Override
 	public void run() {// 스레드
-		while (start_count > count_down) {
-			start_count--;
-			try {
+		try {
+			while (start_count > count_down) {
+				start_count--;
 				Thread.sleep(1000);// 1초
-			} catch (InterruptedException e) {
-
 			}
+		} catch (InterruptedException e) {
+		} finally {
+			System.out.println("시간이 종료 되었습니다.");
+			System.out.println("총 스코어 " + score + "입니다. 축하합니다");
+
 		}
 		// start_count 끝나면 실행
 		System.out.println("시간이 종료 되었습니다.");
@@ -44,8 +52,7 @@ public class Hi99 extends Thread {
 	}
 
 	public void hi_start() {
-		Random random = new Random();
-		Scanner sc = new Scanner(System.in);
+		String pattern = "^[0-9]*$";// 숫자만
 		Hi99 thread = new Hi99();
 
 		System.out.println("구구단 게임을 시작합니다.");
@@ -53,21 +60,24 @@ public class Hi99 extends Thread {
 		System.out.println("시작!");
 
 		thread.start();// 스레드 실행 (run)
+
 		while (true) {
 			x = random.nextInt(9) + 1;
 			y = random.nextInt(9) + 1;
 			answer = x * y;// 곱셈 결과 값
-
 			System.out.print(x + " x " + y + " => ");
+			z = sc.nextLine();// 사용자 입력
+			Boolean regex = Pattern.matches(pattern, z);
+			if (!regex) {
+				System.out.println("정수만 입력해주세요");
+				continue;
 
-			z = sc.nextInt();// 사용자 입력
-
-			if (z == answer) {
+			} else if (answer == Integer.valueOf(z)) {
 				score += 10;
-				System.out.println("정답입니다! " + plus + "점 추가 " + " 총점 =>" + score);
+				System.out.println("정답입니다! " + plus + "점 추가 " + "총점 =>" + score);
 
-			} else if (z != answer) {
-				System.out.println("오답입니다! 정답은 : " + answer + " 입니다");
+			} else {
+				System.out.println("오답입니다! 정답-> " + answer + " 총점 =>" + score + " 다음문제");
 
 			}
 		}
