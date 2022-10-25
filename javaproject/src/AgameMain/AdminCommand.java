b95b57a0a4a92be4beb337e91363310d444d0c48
김주환 <juhwan71047@gmail.com>
2022-10-25 13:11:36 +0900

    Log_outController

--- javaproject/src/AgameMain/AdminCommand.java
+++ javaproject/src/AgameMain/AdminCommand.java
@@ -6,6 +6,7 @@ import exploiter.controller.Controller;
 import exploiter.controller.ExitController;
 import exploiter.controller.InsertController;
 import exploiter.controller.LogInController;
+import exploiter.controller.Log_outController;
 import exploiter.controller.UpdateController;
 import rank.controller.RankDeleteController;
 import rank.controller.RankInsertController;
@@ -24,7 +25,7 @@ public class AdminCommand {
 		command.put(2, (Controller) new RankResearchController()); // 랭크검색
 		command.put(3, (Controller) new RankDeleteController()); // 랭크삭제
 		command.put(4, (Controller) new RankInsertController()); // 랭크수정
-		command.put(5, (Controller) new Log_out()); // 로그아웃
+		command.put(5, (Controller) new Log_outController()); // 로그아웃
 
 		adminCommand.put(1, (Controller) new UpdateController()); // 전체 회원관리
 		adminCommand.put(2, (Controller) new RankResearchController()); // 랭크검색
