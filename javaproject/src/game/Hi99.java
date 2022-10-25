e262b081c3a053ce6af6fb41e94d910519306e07
김주환 <juhwan71047@gmail.com>
2022-10-25 15:55:56 +0900

    Hi99_정규식

--- javaproject/src/game/Hi99.java
+++ javaproject/src/game/Hi99.java
@@ -10,42 +10,47 @@ import java.util.regex.Pattern;
 //  score;// 총점
 
 public class Hi99 extends Thread {
+	Random random = new Random();
+	Scanner sc = new Scanner(System.in);
+	String pattern = "^[0-9]*$";// 숫자만
+	String val = "123456789"; // 대상문자열
 
 	public static void main(String[] args) {
+		// 테스트 위해서 사용
 		Hi99 hi99 = new Hi99();
 		hi99.hi_start();
 	}
 
-	static int x;// 앞자리
-	static int y;// 뒷자리
-	static String z;// 사용자입력값
-	static int answer;// 앞*뒤=결과값
+	int x;// 앞자리
+	int y;// 뒷자리
+	String z;// 사용자입력값
+	int answer;// 앞*뒤=결과값
 	static int score;// 총점
-	static int plus = 10;// 추가점수
-	static int count_down = 0;// 카운트 다운
-	static int start_count = 30;// 잔여시간
-	static int serial_Num = 2;// 게임 시리얼넘버
+	int plus = 10;// 추가점수
+	int count_down = 0;// 카운트 다운
+	int start_count = 10;// 잔여시간
+	int serial_Num = 2;// 게임 시리얼넘버
 
 	@Override
 	public void run() {// 스레드
-		while (start_count > count_down) {
-			start_count--;
-			try {
+		try {
+			while (start_count > count_down) {
+				start_count--;
 				Thread.sleep(1000);// 1초
-			} catch (InterruptedException e) {
-
+				// start_count 끝나면 실행
 			}
+			Thread.interrupted();
+		} catch (InterruptedException e) {
+
+		} finally {
+			System.out.println("시간이 종료 되었습니다.");
+			System.out.println("총 스코어 " + score + "입니다. 축하합니다");
 		}
-		// start_count 끝나면 실행
-		System.out.println("시간이 종료 되었습니다.");
-		System.out.println("총 스코어 " + score + "입니다. 축하합니다");
+
 	}
 
 	public void hi_start() {
-		Random random = new Random();
-		Scanner sc = new Scanner(System.in);
 		Hi99 thread = new Hi99();
-		String pattern = "^[0-9]*$";
 
 		System.out.println("구구단 게임을 시작합니다.");
 		System.out.println(start_count + "초 동안 문제를 빠르게 풀어주세요");
@@ -60,18 +65,19 @@ public class Hi99 extends Thread {
 			System.out.print(x + " x " + y + " => ");
 			while (true) {
 				z = sc.nextLine();// 사용자 입력
-				
-				Pattern.matches(pattern, System.out.println(re))
-				if (!(z == p)) {
+				Boolean regex = Pattern.matches(pattern, z);
+				if (!regex) {
+					System.out.println("정수만 입력해주세요");
+					continue;
+
+				} else if (answer == Integer.valueOf(z)) {
+					score += 10;
+					System.out.println("정답입니다! " + plus + "점 추가 " + "총점 =>" + score);
+					break;
+				} else {
+					System.out.println("오답입니다! 정답-> " + answer + " 총점 =>" + score + " 다음문제");
 					break;
 				}
-			}
-			System.out.println();
-			if (answer == Integer.parseInt(z)) {
-				score += 10;
-				System.out.println("정답입니다! " + plus + "점 추가 " + " 총점 =>" + score);
-			} else {
-				System.out.println("오답입니다! 정답 -> " + answer + " 다음문제");
 			}
 		}
 	}
