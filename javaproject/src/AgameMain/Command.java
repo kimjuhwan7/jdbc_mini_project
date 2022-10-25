6b88c5aec483a5092e015db703a1c2ab5310900c
jinsol1997 <mngjs97@gmail.com>
2022-10-25 10:11:59 +0900

    Create LogInController

--- javaproject/src/AgameMain/Command.java
+++ javaproject/src/AgameMain/Command.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import exploiter.controller.Controller;
 import exploiter.controller.ExitController;
+import exploiter.controller.LogInController;
 
 //Main.Command
 //메인.커멘드
@@ -12,7 +13,7 @@ public class Command {
 	public Command() {
 		command = new HashMap<>();
 
-		// commend.put(1, new 로그인());
+		command.put(1, new LogInController());
 		// commend.put(2, new 회원가입());
 		command.put(3, new ExitController());
 
