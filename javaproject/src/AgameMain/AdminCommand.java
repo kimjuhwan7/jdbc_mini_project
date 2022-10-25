a5b8f951625364a5e4d8ef82e23a0271d3adea96
jennakim1007 <jennakim1007@gmail.com>
2022-10-25 12:24:48 +0900

    LoginCommand
    로그인 이후 Command 수정

--- javaproject/src/AgameMain/AdminCommand.java
+++ javaproject/src/AgameMain/AdminCommand.java
@@ -13,16 +13,16 @@ import rank.controller.RankResearchController;
 //Main.Command
 //메인.커멘드
 public class AdminCommand {
-	public Map<Integer, Controller> command;
+	public Map<Integer, Controller> adminCommand;
 
 	public AdminCommand() {
-		command = new HashMap<>();
+		adminCommand = new HashMap<>();
 
-		command.put(1, (Controller) new UpdateController()); // 전체 회원관리
-		command.put(2, (Controller) new RankResearchController()); // 랭크검색
-		command.put(3, (Controller) new RankDeleteController()); // 랭크삭제
-		command.put(4, (Controller) new RankInsertController()); // 랭크수정
-		// command.put(5, new ); // 로그아웃
+		adminCommand.put(1, (Controller) new UpdateController()); // 전체 회원관리
+		adminCommand.put(2, (Controller) new RankResearchController()); // 랭크검색
+		adminCommand.put(3, (Controller) new RankDeleteController()); // 랭크삭제
+		adminCommand.put(4, (Controller) new RankInsertController()); // 랭크수정
+		// command.put(5, new ); // 로그아웃 -> 메인메뉴로 이동 
 
 	}
 
