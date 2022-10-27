09ef4389187cbca6856e17e49bd27580413597ba
김주환 <juhwan71047@gmail.com>
2022-10-27 11:18:09 +0900

    Update Hi99.java

--- javaproject/src/game/Hi99.java
+++ javaproject/src/game/Hi99.java
@@ -42,7 +42,10 @@ public class Hi99 {
 			System.out.println("만점입니다! 대단합니다!");
 			reGame();
 		} else if (score < 100) {// 100점 미만일 경우
-			System.out.println("최종점수는 100점 만점 중 " + score + " 점입니다." + "\n아쉽게만점을 받지 못했습니다.\n");
+			System.out.println("최종점수는 100점 만점 중 " + score + " 점입니다.");
+			System.out.println("아쉽게만점을 받지 못했습니다.");
+			System.out.println();
+
 			reGame();
 		}
 	}
